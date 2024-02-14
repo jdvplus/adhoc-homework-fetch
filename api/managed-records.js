4ccbafeb1063ee6b054404f1aed4ba9f93270f67
Jaime de Venecia <jaime.devenecia@codesmith.io>
2024-02-14 15:46:02 -0800

    add pseudo-code

--- api/managed-records.js
+++ api/managed-records.js
@@ -1,8 +1,37 @@
-import URI from "urijs";
+import URI from 'urijs';
 
 // /records endpoint
-window.path = "http://localhost:3000/records";
+window.path = 'http://localhost:3000/records';
 
-// Your retrieve function plus any additional functions go here ...
+const uri = new URI('http://localhost:3000/records?limit=10&offset=10');
+
+const retrieve = async (options) => {
+  // if options.page === 1, URI should be string + '?limit=10'
+  // if options.page === 2, URI should be string + '?limit=10&offset=10'
+  // if options.page === 3, URI should be string + '?limit=10&offset=20'
+  // ... and so on
+
+  // helper: map options to uri
+  // input: page, colors
+  // output: string representing URI
+  const mapOptionsToURI = () => {
+
+  }
+
+
+  try {
+    const res = await fetch(uri);
+    const data = await res.json();
+
+    console.log('data', data);
+
+    return data;
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+// example:
+retrieve({ page: 2, colors: ["red", "brown"] })
 
 export default retrieve;
