bcb2794e0e29a890a09b2af4b094831874a85dfc
Jaime de Venecia <jaime.devenecia@codesmith.io>
2024-02-14 16:23:21 -0800

    implement dynamic URI functionality with pagination & color selections

--- api/managed-records.js
+++ api/managed-records.js
@@ -3,16 +3,22 @@ import URI from 'urijs';
 // /records endpoint
 window.path = 'http://localhost:3000/records';
 
-const uri = new URI('http://localhost:3000/records?limit=10&offset=0');
+const uri = new URI(
+  'http://localhost:3000/records?limit=10&offset=0&color[]=red'
+);
 
-/* notes */
+/* notes/observations */
 
-// if options.page === 1, URI should be string + '?limit=10'
-// if options.page === 2, URI should be string + '?limit=10&offset=10'
-// if options.page === 3, URI should be string + '?limit=10&offset=20'
+// PAGINATION
+// if options.page === 1, URI should be string + '&offset=0'
+// if options.page === 2, URI should be string + '&offset=10'
+// if options.page === 3, URI should be string + '&offset=20'
 // ... and so on
 
-
+// COLORS
+// if options.colors includes 'red', URI should be string + '&color[]=red'
+// if options.colors includes 'red' & 'brown', URI should be string + '&color[]=red&color[]=brown'
+// ... and so on
 
 const retrieve = async (options) => {
   // helper fn: get page
@@ -26,10 +32,14 @@ const retrieve = async (options) => {
   // input: array of strings
   // output: string representing add'l query params
   const getColors = (colorsArray) => {
+    let colorParams = '';
 
-    
-    return;
-  }
+    for (const color of colorsArray) {
+      colorParams += `&color[]=${color}`;
+    }
+
+    return colorParams;
+  };
 
   // helper fn: map options to uri
   // input: page (num), colors (array)
@@ -37,22 +47,27 @@ const retrieve = async (options) => {
   const mapOptionsToURI = (page, colors) => {
     const baseURIString = 'http://localhost:3000/records?limit=10';
 
-    return `${baseURIString}${getPage(page)}`;
+    if (!page) return baseURIString + getColors(colors);
+    if (!colors) return baseURIString + getPage(page);
+    if (!page && !colors) return baseURIString;
+
+    return baseURIString + getPage(page) + getColors(colors);
   };
 
   try {
-    const res = await fetch(uri);
+    const res = await fetch(mapOptionsToURI(options.page, options.colors));
     const data = await res.json();
 
     console.log('data', data);
+    console.log('data length', data.length);
 
     return data;
   } catch (err) {
-    console.log(err);
+    console.log('error:', err);
   }
 };
 
 // example:
-retrieve({ page: 2, colors: ['red', 'brown'] });
+retrieve({});
 
 export default retrieve;
